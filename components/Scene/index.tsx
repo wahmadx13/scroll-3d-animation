8bd9aaf5354282959fa8ec3bda2f30efdc1319ec
Waseem <wasim_ahmad2003@yahoo.com>
2024-04-25 21:38:42 +0500

    fixed not glowing issue

--- components/Scene/index.tsx
+++ components/Scene/index.tsx
@@ -13,6 +13,7 @@ const Loader = () => {
 const Scene = () => {
   return (
     <Canvas gl={{ antialias: true }} dpr={[1, 1.5]} className='relative h-svh'>
+      <directionalLight position={[-5, -5, 5]} intensity={6} />
       <Suspense fallback={<Loader />}>
         <ScrollControls damping={0.5} pages={10}>
           <Model />
